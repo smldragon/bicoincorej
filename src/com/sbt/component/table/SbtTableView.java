04fe6b0b429b47a5a14fb83377ea001186e446e4
Frank zheng <smldragon@hotmail.com>
2018-02-02 20:02:34 -0500

    initial commit of table package

--- src/com/sbt/component/table/SbtTableView.java
+++ src/com/sbt/component/table/SbtTableView.java
@@ -1,6 +1,13 @@
 package com.sbt.component.table;
 
+import com.sun.javafx.scene.control.skin.TableHeaderRow;
+import javafx.collections.ListChangeListener;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.util.List;
 
 public class SbtTableView<S extends SbtTableRowData> extends TableView<S> {
 
@@ -26,12 +33,63 @@ public class SbtTableView<S extends SbtTableRowData> extends TableView<S> {
         initStatus = true;
 
     }
+    public TablePropertyInitializer getTablePropertyInitializer() {
+        return tablePropertyInitializer;
+    }
+
+    public void setTablePropertyInitializer(TablePropertyInitializer tablePropertyInitializer) {
+        this.tablePropertyInitializer = tablePropertyInitializer;
+    }
     private void installRowNumberColumn() {
         if ( ! rowNumberColStatus) {
             rowNumberColStatus = true;
             doInstallRowNumberColumn();
         }
     }
+    protected void initComponents() {
+
+        tableCellsSelector = new TableCellsSelector(this);
+
+        if ( null != tablePropertyInitializer) {
+            tablePropertyInitializer.initTableProperty(this);
+        }
+
+        setTableMenuButtonVisible(true);
+
+        setRowFactory( new ExpandableTableRowFactory<S>());
+
+        if ( null == promptPlaceHolder ) {
+            showPlaceHolderText(null);
+        }
+
+        getColumns().addListener((ListChangeListener.Change<? extends TableColumn<S, ?>> c)-> {
+            if ( 0 == getColumns().size()) {
+                rowNumbeInstalled = false;
+            }
+        });
+
+        TableHeaderRow thr = Utils.getTableHeaderRow(getSkin());
+        if ( null != thr) {
+            ContextMenu contextMenu = Utils.getContextMenu(thr);
+            List<MenuItem> menuItems = contextMenu.getItems();
+            contextMenu.setOnShown( (event) -> {
+                for(MenuItem item: menuItems)  {
+                    standarizeCheckMenuItemText(item);
+                    if ( RowNoColTitle.equals(item.getText())) {
+                        item.setDisable(true);
+                    }
+                }
+                SbtTableCacheMng.addColumnConfigChangedTable(SbtTableView.this);
+            });
+        }
+
+        installKeyListeners();
+
+        checkTableCellFactory();
+    }
+    private boolean rowNumbeInstalled = false;
+    private TablePropertyInitializer tablePropertyInitializer;
+    private TableCellsSelector tableCellsSelector;
     private boolean initStatus = false;
     private boolean inRestoredFromCache = false;
     private boolean rowNumberColStatus = false;
