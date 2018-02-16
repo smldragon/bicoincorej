97b1338df000d726fbd227e26906b0e7865574ae
Frank zheng <smldragon@hotmail.com>
2018-02-16 00:00:47 -0500

    initial commit of table package with errors

---
+++ src/com/sbt/utils/Amount.java
@@ -0,0 +1,44 @@
+package com.sbt.utils;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+public class Amount extends Number {
+
+    private final DecimalFormat formatter = new DecimalFormat("#,##0.00");
+    private final Double value;
+    public Amount(String sValue) throws ParseException {
+        value = formatter.parse(sValue).doubleValue();
+    }
+    public Amount(int iValue) {
+        value = new Double(iValue);
+    }
+    public Amount(float fValue) {
+        value = new Double(fValue);
+    }
+    public Amount(double dValue) {
+        value = dValue;
+    }
+    public Amount(long lValue) {
+        value = new Double(lValue);
+    }
+    @Override
+    public int intValue() {
+        return value.intValue();
+    }
+
+    @Override
+    public long longValue() {
+        return value.longValue();
+    }
+
+    @Override
+    public float floatValue() {
+        return value.floatValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return value.doubleValue();
+    }
+}
